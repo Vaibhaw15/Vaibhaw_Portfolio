1f6dcbd4b81ec270c69169f5cd620d811f30c347
Vaibhaw Soni <vaibhawsoni900@gmail.com>
2025-05-17 14:09:48 +0000

    Change the colour of the Ui make it dark and good

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
         <Header />
         <main className="flex-1">
